615c9a9bb893e39efc829e37fd9e469a8a278729
Amirhossein Zandi <blackviper933@gmail.com>
2023-09-22 18:42:26 +0330

    new

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import MoviesData from "@/components/MoviesData/MoviesData";
 
 const page = () => {
   return (
-    <section className="content">
+    <section className="content py-9">
       <div className="container mx-auto px-4 flex flex-col">
         <MoviesData />
       </div>
--- components/MoviesData/MoviesData.jsx
+++ components/MoviesData/MoviesData.jsx
@@ -1,26 +1,63 @@
 "use client"
 
-const getData = async () => {
-    const data = await fetch("https://moviesapi.ir/api/v1/movies?page={page}")
-    return data.json()
-}
+import { useEffect, useState } from "react"
+import Image from "next/image"
 
 
 
 
+const MoviesData = () => {
+    const [filmsData, setFilmsData] = useState([])
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
 
-const MoviesData = async () => {
-    const { data } = await getData()
-    console.log(data[0]);
+    useEffect(() => {
+
+        const getData = async () => {
+            setLoading(true)
+
+            try {
+                const data = await fetch("https://moviesapi.ir/api/v1/movies?page={page}")
+                const response = await data.json()
+                setFilmsData(response.data)
+                setLoading(false)
+            }
+            catch (error) {
+                setError(true)
+                setLoading(false)
+            }
+        }
+        getData()
+    }, [])
+
+
     return (
         <>
-            <section className="content__head text-white mt-5">
+            <section className="content__head text-white">
                 <h1>New items</h1>
                 <p className="mt-3">NEW RELEASES <i className="bi bi-filter-left"></i></p>
             </section>
 
-            <section className="movie-items">
-                <h4 className="text-white">hiiiii</h4>
+            <p className="text-white">{loading && ("loading...")}</p>
+            <p className="text-rose-400">{error && ("an error occured")}</p>
+
+
+            <section className="movie-items flex flex-wrap justify-evenly gap-3">
+
+                {
+                    filmsData && filmsData.map(items => (
+                        <div key={items.id} className="text-white flex flex-col w-[45%] sm:w-[30%] md:w-[23%] lg:w-[18%]">
+                            <figure className=" rounded-md overflow-hidden">
+                                <Image src={items.poster} width={200} height={200} alt={items.title} property="true" />
+                            </figure>
+                            <figcaption className="py-2">{items.title}</figcaption>
+                            <p>{items.genres.join(" , ")}</p>
+                            <span className={`imdb-rate ${items.imdb_rating>=9.0 && ("imdb-rate-border-green") || items.imdb_rating>8.8 && ("imdb-rate-border-yellow") || items.imdb_rating<=8.8 && ("imdb-rate-border-red")}`}>{items.imdb_rating}</span>
+                        </div>
+                    ))
+
+                }
+
             </section>
         </>
     );

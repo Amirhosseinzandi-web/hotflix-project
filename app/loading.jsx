d693b7a6e72aa613e1a8e5f1c0876a6ae8286f68
Amirhossein Zandi <blackviper933@gmail.com>
2023-09-30 18:03:46 +0330

    add loading

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
+
 const loading = () => {
     return (
         <div className="native-loading">
-            loading...
+            <Image priority={true} width={100} height={100} src={"/loading.gif"} alt="loading"/>
         </div>
     );
 }
